bae43d0f5fa9524dae1f44aaacc1fb9d3e190717
timlebrun111 <tdlebrun@email.neit.edu>
2022-01-25 18:37:38 -0500

    Week 3 Class

---
+++ BlockBuster/BlockBuster/BlockBusterBasicFunctions.cs
@@ -0,0 +1,89 @@
+﻿using BlockBuster.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockBuster
+{
+    public class BlockBusterBasicFunctions
+    {
+
+        public static Movie GetMovieById(int id)
+        {
+            try
+            {
+
+                using(var context = new SE407_BlockBusterContext())
+                {
+                    return context.Movies.Find(id);
+                }
+
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public static List<Movie> GetAllMovies()
+        {
+            try
+            {
+
+                using (var context = new SE407_BlockBusterContext())
+                {
+                    return context.Movies.ToList();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public static List<Movie> GetAllCheckedOutMovies()
+        {
+            try
+            {
+
+                using (var context = new SE407_BlockBusterContext())
+                {
+                    return context.Movies
+                        .Join(context.Transactions,
+                        m => m.MovieId,
+                        t => t.MovieId,
+                        (m, t) => new
+                        {
+                            MovieId = m.MovieId,
+                            Title = m.Title,
+                            ReleaseYear = m.ReleaseYear,
+                            GenreId = m.GenreId,
+                            DirectorId = m.DirectorId,
+                            CheckedIn = t.CheckedIn
+                        }).Where(w => w.CheckedIn == "N")
+                        .Select(m => new Movie
+                        {
+
+                            MovieId = m.MovieId,
+                            Title = m.Title,
+                            ReleaseYear = m.ReleaseYear,
+                            GenreId = m.GenreId,
+                            DirectorId = m.DirectorId
+                        }).ToList();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+    }
+}
---
+++ BlockBuster/BlockBusterTest/BlockBusterBasicFunctionsTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using BlockBuster;
+
+namespace BlockBusterTest
+{
+    public class BlockBusterBasicFunctionsTest
+    {
+        [Fact]
+        public void  GetMovieByIdTest()
+        {
+            var result = BlockBusterBasicFunctions.GetMovieById(11);
+            Assert.True(result.Title.ToLower() == "vertigo");
+            Assert.True(result.ReleaseYear == 1958);
+        }
+
+        [Fact]
+        public void GetAllMoviesTest()
+        {
+            var result = BlockBusterBasicFunctions.GetAllMovies();
+            Assert.True(result.Count == 49);
+        }
+
+        [Fact]
+        public void GetAllCheckedOutMoviesTest()
+        {
+            var result = BlockBusterBasicFunctions.GetAllCheckedOutMovies();
+            Assert.True(result.Count == 3);
+        }
+    }
+}
