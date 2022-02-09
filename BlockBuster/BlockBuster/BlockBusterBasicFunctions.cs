e9ba553cbfe86828d3154130b8bd5e663ccda068
timlebrun111 <tdlebrun@email.neit.edu>
2022-02-08 20:29:46 -0500

    Midterm

--- BlockBuster/BlockBuster/BlockBusterBasicFunctions.cs
+++ BlockBuster/BlockBuster/BlockBusterBasicFunctions.cs
@@ -9,7 +9,7 @@ namespace BlockBuster
 {
     public class BlockBusterBasicFunctions
     {
-
+        //Retrieves a list of movies based on a specific ID.
         public static Movie GetMovieById(int id)
         {
             try
@@ -27,6 +27,8 @@ namespace BlockBuster
                 return null;
             }
         }
+        
+        //Retrieves a list of all movies.
 
         public static List<Movie> GetAllMovies()
         {
@@ -45,6 +47,8 @@ namespace BlockBuster
                 return null;
             }
         }
+
+        //Retrieves a list of all checked out movies.
 
         public static List<Movie> GetAllCheckedOutMovies()
         {
@@ -84,6 +88,78 @@ namespace BlockBuster
                 return null;
             }
         }
+
+
+        //Gets all movies by a specific Directors Last name.
+        public static List<Movie> GetAllMoviesByDirectorsLastName(string dLastName)
+        {
+            try
+            {
+                using var context = new SE407_BlockBusterContext();
+                return context.Movies.Join(context.Directors, movie => movie.DirectorId, director => director.DirectorId,
+                    (movie, director) => new
+                    {
+                        movie.MovieId,
+                        movie.Title,
+                        movie.ReleaseYear,
+                        movie.GenreId,
+                        movie.DirectorId,
+                        DirectorLastName = director.LastName
+
+                    }).Where(movie => movie.DirectorLastName.ToLower() == dLastName.ToLower())
+                    .Select(m => new Movie()
+                    {
+                        MovieId = m.MovieId,
+                        Title = m.Title,
+                        ReleaseYear = m.ReleaseYear,
+                        GenreId = m.GenreId,
+                        DirectorId = m.DirectorId
+                    }).ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        //Retrieves a list of movies based on the Genre.
+        public static List<Movie> GetAllMoviesByGenre(string gDescription)
+        {
+            try
+            {
+                using var context = new SE407_BlockBusterContext();
+                return context.Movies.Join(context.Genres, movie => movie.GenreId, genre => genre.GenreId,
+                        (movie, genre) => new
+                        {
+
+                            movie.MovieId,
+                            movie.Title,
+                            movie.ReleaseYear,
+                            movie.GenreId,
+                            movie.DirectorId,
+                            GenreDescription = genre.GenreDescr
+
+
+                        }).Where(movie => movie.GenreDescription.ToLower() == gDescription.ToLower())
+                    .Select(m => new Movie()
+                    {
+                        MovieId = m.MovieId,
+                        Title = m.Title,
+                        ReleaseYear = m.ReleaseYear,
+                        GenreId = m.GenreId,
+                        DirectorId = m.DirectorId
+                    }).ToList();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+
+        }
+
+        
 
     }
 }
